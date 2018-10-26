7ab7ba87770a927919a6d659b60b0173f1aba46a
GMRDL5 <gmrstudent@outlook.com>
2018-10-25 19:38:00 -0700

    Add project files.

---
+++ MeganMastermindWinForm/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MeganMastermindWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Think of 4 different numbers";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(27, 42);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userTextBox.TabIndex = 1;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(133, 39);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 2;
+            this.guessButton.Text = "Guess";
+            this.guessButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Button guessButton;
+    }
+}
+
---
+++ MeganMastermindWinForm/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeganMastermindWinForm
+{
+    public partial class Form1 : Form
+    {
+        int[] master = new int[4];
+        int[] userArray = new int[4];
+
+        //create numbers
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            //for one number only
+            int num = int.Parse(userTextBox.Text);
+
+            
+        }
+    }
+}
